aae4941e4aba7010a9387d9ba4474b57bddd1835
Dmitry Kalinin <kalinin.mitko@gmail.com>
2024-08-18 16:04:43 +0300

    CI/CD #1

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="crel",
     description="fast, templateless html generation",
     keywords="python, html, simple, fast",
     url="https://github.com/null-none/crel",
-    packages=[
-        "types",
-    ],
-    namespace_packages=["src"],
+    packages=find_packages(),
     classifiers=["Framework :: Django"],
 )
